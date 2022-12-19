9126a27d9e431976da07de2bbd42c88165f9145a
Yomar Gutierrez <yomar.guti@gmail.com>
2022-12-19 09:45:24 -0500

    Creating queries and loading error states

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -1,13 +1,24 @@
-import { Posts } from "./Posts";
-import "./App.css";
+import { Posts } from './Posts';
+import './App.css';
+
+// import React Query client
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+// create a client
+const queryClient = new QueryClient();
 
 function App() {
   return (
     // provide React Query client to App
-    <div className="App">
-      <h1>Blog Posts</h1>
-      <Posts />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Blog Posts</h1>
+        <Posts />
+      </div>
+      {/* React Query Devtools */}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
+import { useState } from 'react';
+import { useQuery } from 'react-query';
 
-import { PostDetail } from "./PostDetail";
+import { PostDetail } from './PostDetail';
 const maxPostPage = 10;
 
 async function fetchPosts() {
-  const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
-  );
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0');
   return response.json();
 }
 
@@ -15,17 +14,15 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError } = useQuery('posts', fetchPosts);
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error!</div>;
 
   return (
     <>
       <ul>
         {data.map((post) => (
-          <li
-            key={post.id}
-            className="post-title"
-            onClick={() => setSelectedPost(post)}
-          >
+          <li key={post.id} className="post-title" onClick={() => setSelectedPost(post)}>
             {post.title}
           </li>
         ))}
